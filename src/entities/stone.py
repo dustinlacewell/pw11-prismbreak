8e08f08c5ce67ecd2413eb7189bde9650099d925
Dustin Lacewell <ldlework@vector.(none)>
2010-08-24 08:06:43 -0500

    Shimer tiles.

--- src/entities/stone.py
+++ src/entities/stone.py
@@ -7,7 +7,7 @@ class Stone(Entity):
     name = 'stone'
     icon = "#"
     fg = Color(90, 104, 110)
-    bg = Color(153, 182, 196)
+    bg = Color(73,58,67)
     block = True
 
 exported_class = Stone

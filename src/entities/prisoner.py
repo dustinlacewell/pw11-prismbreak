936d0e2fec64104a92ae1a097a996ab2d9e09422
Dustin <ldlework@ldlework-desktop.(none)>
2010-08-24 22:08:17 -0500

    Added prisoner

---
+++ src/entities/prisoner.py
@@ -0,0 +1,28 @@
+import random
+
+from pytcod import *
+from src.entities import Entity
+
+
+class Prisoner(Entity):
+    name = 'prisoner'
+    icon = "P"
+    fg = Color(255, 0, 0)
+    bg = Color(31, 33, 37)
+    block = True
+
+    def update(self, game):
+        level = game.level
+        player = game.player
+        actions = [
+            self.move_left,
+            self.move_right,
+            self.move_up,
+            self.move_down,
+        ]
+
+        action = random.choice(actions)
+        action(game)
+        
+
+exported_class = Prisoner
